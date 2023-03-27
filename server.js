441e7924ebf4e41e10b78ab695caffa0be321a71
Edward Baker <eddean@email.unc.edu>
2023-03-26 23:45:21 -0400

    finished adding in the other endpoints and added sserver response status.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
 
 // Edward Baker
+
+// I learned that: The order in which you define your endpoints in the file can affect how they are processed by your server.
+// In most web frameworks, including Express, routes are processed in the order they are defined in the code.
+// This means that if a request matches multiple routes, the first matching route will be used.
 
 // Import modules from a03
 import {rps, rpsls} from "./lib/rpsls.js";
@@ -7,26 +12,62 @@ import {rps, rpsls} from "./lib/rpsls.js";
 import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 // Setting up express
-const express = require('express');
+import express from "express";
 const app = express();
 // Defining port
 const PORT = args.port || 5000;
 
-
-// Required API Endpoints Below
-
-// Default Endpoint
-app.use((req, res) => {
-    res.status(404).send('404 Not Found');
-  });
-
 // Check Endpoint
 app.get('/app/', (req, res) => {
-  res.sendStatus(200);
+  res.status(200).send('200 OK');
 });
 
+// Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}
+app.get('app/rps/',(req,res) => {
+  res.status(200).send(rps());
+});
+
+// Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
+app.get('app/rpsls/',(req,res) => {
+  res.status(200).send(rpsls());
+});
+
+// Endpoint /app/rps/play/, takes shot=(rock|paper|scissors) (URLEncoded)
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.query.shot));
+});
+
+// Endpoint /app/rps/play/, takes shot=(rock|paper|scissors) (JSON)
+app.get('/app/rps/play/', (req, res) => {
+	res.status(200).send(rps(req.body.shot));
+});
+
+// Endpoint /app/rpsls/play/, takes shot=(rock|paper|scissors) (URLEncoded)
+app.get('/app/rpsls/play/', (req, res) => {
+	res.status(200).send(rpsls(req.query.shot));
+});
+
+// Endpoint /app/rps/play/, takes shot=(rock|paper|scissors) (JSON)
+app.get('/app/rpsls/play/', (req, res) => {
+	res.status(200).send(rpsls(req.body.shot));
+});
+
+// Endpoint /app/rpsls/play/(rock|paper|scissors)/
+app.get('/app/rps/play/:shot', (req, res) => {
+	res.status(200).send(rps(req.params.shot));
+});
+
+// Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
+app.get('/app/rpsls/play/:shot', (req, res) => {
+	res.status(200).send(rpsls(req.params.shot));
+});
+
+// Default Endpoint
+app.use('*', (req, res) => {
+  res.status(404).send('404 Not Found');
+});
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
